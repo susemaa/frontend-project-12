7e21038dc2e18c9e9c2b5b44854bf959b551d411
Arseny Radoslavov <rad.archeg@gmail.com>
2023-10-19 18:41:21 +0700

    added autoscroll to bottom at messagebox

--- frontend/src/Components/Messages/index.jsx
+++ frontend/src/Components/Messages/index.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useRef, useEffect } from 'react';
 import { useSelector } from 'react-redux';
 import { Col } from 'react-bootstrap';
 import Header from './MessagesHeader.jsx';
@@ -11,10 +11,21 @@ const Messages = () => {
   const currentChannel = channels.find((ch) => ch.id === currentChannelId);
 
   const currentMessages = messages.filter((message) => message.channelId === currentChannelId);
+  const messageBoxRef = useRef(null);
+
+  const scrollToBottom = () => {
+    if (messageBoxRef.current) {
+      messageBoxRef.current.scrollTop = messageBoxRef.current.scrollHeight;
+    }
+  };
+  useEffect(() => {
+    scrollToBottom();
+  }, [currentMessages]);
+
   return (
     <Col className="p-0 h-100 d-flex flex-column">
       <Header currentChannel={currentChannel} messagesAmount={currentMessages.length} />
-      <div id="message-box" className="chat-messages overflow-auto px-5">
+      <div id="message-box" className="chat-messages overflow-auto px-5" ref={messageBoxRef}>
         {currentMessages.map((message) => (<Message key={message.id} message={message} />))}
       </div>
       <SendingWindow currentChannel={currentChannel} />
